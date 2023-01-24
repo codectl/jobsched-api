709cf8c0f6819f44cd03e283d43fc0a639ff558d
Renato Damas <me@renatodamas.com>
2023-01-24 17:11:04 +0000

    chore: add metadata

--- src/__metadata__.py
+++ src/__metadata__.py
@@ -1,8 +1,9 @@
 from importlib import metadata
 
 
-__title__: str = metadata.metadata(__package__)["name"]
-__description__: str = metadata.metadata(__package__)["summary"]
-__version__: str = metadata.metadata(__package__)["version"]
-__author__: str = metadata.metadata(__package__)["author"]
-__license__: str = metadata.metadata(__package__)["license"]
+pkg_name = "jobsched-api"
+__title__: str = metadata.metadata(pkg_name)["name"]
+__description__: str = metadata.metadata(pkg_name)["summary"]
+__version__: str = metadata.metadata(pkg_name)["version"]
+__author__: str = metadata.metadata(pkg_name)["author"]
+__license__: str = metadata.metadata(pkg_name)["license"]
