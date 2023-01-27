d03b8d484a9917d610a4f131ad7ad8d852196c3b
Renato Damas <me@renatodamas.com>
2023-01-27 15:13:02 +0000

    chore: add modules for auth

---
+++ src/api/auth.py
@@ -0,0 +1,40 @@
+from functools import wraps
+
+from flask import g, request
+from flask_restful import abort
+from werkzeug.local import LocalProxy
+
+from src.services.auth import AuthSvc
+
+
+# proxy to load username
+current_username = LocalProxy(lambda: load_user())
+
+
+def load_user():
+    """Load user from request context."""
+    return getattr(g, "username", None)
+
+
+def requires_auth(schemes=("basic",)):
+    """Validate endpoint against given authorization schemes.
+    Fail if authorization properties are missing or are invalid."""
+
+    def wrapper(func):
+        @wraps(func)
+        def decorated(*args, **kwargs):
+            if "basic" in schemes:
+                auth = request.authorization
+                if auth and AuthSvc.authenticate(
+                    username=auth.username, password=auth.password
+                ):
+                    g.username = auth.username
+                    return func(*args, **kwargs)
+            elif "bearer" in schemes:
+                raise NotImplementedError
+
+            abort(401, code=401, reason="Unauthorized")
+
+        return decorated
+
+    return wrapper
