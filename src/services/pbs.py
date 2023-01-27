109d95551dbb21ee996b7b3865d4b8446d614657
Renato Damas <me@renatodamas.com>
2023-01-27 14:11:57 +0000

    refactor: remove docs for abstract methods #minor

--- src/services/pbs.py
+++ src/services/pbs.py
@@ -3,11 +3,8 @@ from src.services.sched import Sched
 
 
 class PBS(Sched):
-
     def qstat(self, job_id=None, status=None):
-        """Check for a job given job properties."""
         pass
 
     def qsub(self, props: Job):
-        """Submit a job to the scheduler based on given job properties."""
-        raise NotImplementedError
+        pass
