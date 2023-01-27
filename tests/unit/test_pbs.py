422c6a9cbab7a232bed9f184223e7861322ef3a6
Renato Damas <me@renatodamas.com>
2023-01-27 10:41:58 +0000

    test(unit): pbs service

---
+++ tests/unit/test_pbs.py
@@ -0,0 +1,23 @@
+import pytest
+
+from src.services.pbs import PBS
+from src.models.job import Job
+
+
+@pytest.fixture(scope="class")
+def pbs():
+    return PBS
+
+
+@pytest.fixture(scope="class")
+def job():
+    return Job(
+        name="PBS job",
+        queue="queue",
+    )
+
+
+class TestPBS:
+
+    def test_qsub(self, pbs, job):
+        job.dict(exclude_none=True)
