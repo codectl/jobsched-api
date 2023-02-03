745b460441a894ad47d118de81183852d366c3c7
Renato Damas <me@renatodamas.com>
2023-02-03 19:18:02 +0000

    test(unit): move data to conftest #wip

--- tests/unit/test_job.py
+++ tests/unit/test_job.py
@@ -1,87 +1,47 @@
-import pytest
+from datetime import datetime
 
-from src.models.job import Job, JobStatus
-
-
-@pytest.fixture(scope="class")
-def raw_data():
-    return {
-        "timestamp": 1479277336,
-        "pbs_version": "14.1",
-        "pbs_server": "vbox",
-        "Jobs": {
-            "1.vbox": {
-                "Job_Name": "STDIN",
-                "Job_Owner": "user@vbox",
-                "job_state": "Q",
-                "queue": "workq",
-                "server": "vbox",
-                "Checkpoint": "u",
-                "ctime": "Fri Nov 11 17:57:05 2022",
-                "Error_Path": "/tmp/STDIN.e1",
-                "Hold_Types": "n",
-                "Join_Path": "n",
-                "Keep_Files": "n",
-                "Mail_Points": "a",
-                "mtime": "Fri Nov 11 17:57:05 2022",
-                "Output_Path": "/tmp/STDIN.o1",
-                "Priority": 0,
-                "interactive": False,
-                "qtime": "Fri Nov 11 17:57:05 2022",
-                "Rerunable": True,
-                "Resource_List": {
-                    "ncpus": 1,
-                    "nodect": 1,
-                    "place": "pack",
-                    "select": "1:ncpus=1",
-                },
-                "schedselect": "1:ncpus=1",
-                "substate": 10,
-                "Variable_List": {
-                    "PBS_O_HOME": "/root",
-                    "PBS_O_LANG": "en_US.utf8",
-                    "PBS_O_LOGNAME": "user",
-                    "PBS_O_PATH": "/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin",
-                    "PBS_O_MAIL": "/var/spool/mail/user",
-                    "PBS_O_SHELL": "/bin/bash",
-                    "PBS_O_WORKDIR": "/home/user/Tasks/overlay",
-                    "PBS_O_SYSTEM": "Linux",
-                    "PBS_O_QUEUE": "workq",
-                    "PBS_O_HOST": "vbox",
-                },
-                "euser": "root",
-                "egroup": "root",
-                "queue_rank": 1,
-                "queue_type": "E",
-                "comment": "Not Running: Not enough free nodes available",
-                "etime": "Fri Nov 11 17:57:05 2022",
-                "Submit_arguments": "-- /bin/sleep 1000",
-                "executable": "<jsdl-hpcpa:Executable>/bin/sleep</jsdl-hpcpa:Executable>",
-                "argument_list": "<jsdl-hpcpa:Argument>1000</jsdl-hpcpa:Argument>",
-                "project": "_pbs_project_default",
-                "Account_Name": "pbs_account",
-            }
-        },
-    }
+from src.models.job import JobStatus
 
 
-def test_job_deserializer(raw_data):
-    job_data = next(iter(raw_data["Jobs"].values()))
-    job = Job.parse_obj(job_data)
+def test_qsub_deserializer(qsub_job):
+    job = qsub_job
     assert job.name == "STDIN"
-    assert job.owner == "user@vbox"
-    assert job.status == JobStatus.QUEUE
-    assert job.queue == "workq"
-    assert job.server == "vbox"
+    assert job.queue == "testq"
     assert job.submit_args == "-- /bin/sleep 1000"
     assert job.stdout_path == "/tmp/STDIN.o1"
     assert job.stderr_path == "/tmp/STDIN.e1"
-    assert job.resources.select == "1:ncpus=1"
-    assert job.resources.cpu == 1
-    assert job.resources.node_count == 1
+    assert job.resources.cpu == 5
+    assert job.resources.gpu == 2
+    assert job.resources.node_count == 2
     assert job.resources.place == "pack"
-    assert job.comment == "Not Running: Not enough free nodes available"
+    assert job.resources.walltime == "02:00:00"
     assert job.account == "pbs_account"
     assert job.project == "_pbs_project_default"
     assert job.interactive is False
     assert job.rerunable is True
+
+
+def test_qstat_deserializer(qstat_job):
+    job = qstat_job
+
+    # qsub fields only
+    assert job.name == "STDIN"
+    assert job.queue == "workq"
+    assert job.submit_args == "-X -I -l select=1:ncpus=24:mem=32gb -l walltime=24:00:00"
+    assert job.stdout_path == "/home/testu/STDIN.o1"
+    assert job.stderr_path == "/home/testu/STDIN.e1"
+    assert job.resources.request.cpu == 24
+    assert job.resources.request.node_count == 1
+    assert job.resources.request.place == "free"
+    assert job.resources.request.walltime == "24:00:00"
+    assert job.account == "pbs_account"
+    assert job.project == "_pbs_project_default"
+    assert job.interactive is True
+    assert job.rerunable is False
+
+    # qstat fields
+    assert job.owner == "testu"
+    assert job.status == JobStatus.RUNNING
+    assert job.server == "pbs01"
+    assert job.comment == "Job run on Jan 01 at 00:00 on (cn01:ncpus=24:mem=33554432kb)"
+    assert job.timeline.created_at == datetime(2023, 2, 3, 10, 41, 52)
