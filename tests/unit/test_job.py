e51d31e52324063bc439c4085208a8a1951b1687
Renato Damas <me@renatodamas.com>
2023-02-06 08:48:31 +0000

    test(unit): add assert clauses #wip

--- tests/unit/test_job.py
+++ tests/unit/test_job.py
@@ -10,6 +10,7 @@ def test_qsub_deserializer(qsub_job):
     assert job.submit_args == "-- /bin/sleep 1000"
     assert job.stdout_path == "/tmp/STDIN.o1"
     assert job.stderr_path == "/tmp/STDIN.e1"
+    assert job.resources.mem == "10gb"
     assert job.resources.cpu == 5
     assert job.resources.gpu == 2
     assert job.resources.node_count == 2
@@ -25,16 +26,12 @@ def test_qsub_deserializer(qsub_job):
 def test_qstat_deserializer(qstat_job):
     job = qstat_job
 
-    # qsub fields only
+    # common fields
     assert job.name == "STDIN"
     assert job.queue == "workq"
     assert job.submit_args == "-X -I -l select=1:ncpus=24:mem=32gb -l walltime=24:00:00"
     assert job.stdout_path == "/home/testu/STDIN.o1"
     assert job.stderr_path == "/home/testu/STDIN.e1"
-    assert job.resources.request.cpu == 24
-    assert job.resources.request.node_count == 1
-    assert job.resources.request.place == "free"
-    assert job.resources.request.walltime == "24:00:00"
     assert job.account == "pbs_account"
     assert job.project == "_pbs_project_default"
     assert job.flags.interactive is True
@@ -45,5 +42,17 @@ def test_qstat_deserializer(qstat_job):
     assert job.owner == "testu"
     assert job.status == JobStatus.RUNNING
     assert job.server == "pbs01"
+    assert job.resources.request.mem == "32gb"
+    assert job.resources.request.cpu == 24
+    assert job.resources.request.node_count == 1
+    assert job.resources.request.place == "free"
+    assert job.resources.request.walltime == "24:00:00"
+    assert job.resources.used.mem == "32gb"
+    assert job.resources.used.cpu == 24
+    assert job.resources.used.walltime == "00:02:00"
     assert job.comment == "Job run on Jan 01 at 00:00 on (cn01:ncpus=24:mem=33554432kb)"
     assert job.timeline.created_at == datetime(2023, 2, 3, 10, 41, 52)
+    assert job.timeline.updated_at == datetime(2023, 2, 3, 10, 41, 52)
+    assert job.timeline.queued_at == datetime(2023, 2, 3, 10, 41, 53)
+    assert job.timeline.ready_at == datetime(2023, 2, 3, 10, 41, 53)
+    assert isinstance(job.extra, dict) is True
