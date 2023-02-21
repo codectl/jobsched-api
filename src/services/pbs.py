ec36e2dd05dce70b3ff1f312c437bb1d11c0e4ab
Renato Damas <me@renatodamas.com>
2023-02-21 17:51:05 +0000

    refactor: create exec method to run shell instruction #wip

--- src/services/pbs.py
+++ src/services/pbs.py
@@ -7,17 +7,15 @@ from src.services.sched import Sched
 
 
 class PBS(Sched):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._bin = os.path.join(self.env["PBS_EXEC"], "bin")
 
     def qstat(self, job_id=None, status=None) -> JobStat:
-        exe = os.path.join(self._bin, "qstat")
-        cmd = " ".join((exe, job_id))
-        data = shell(command=cmd).output(raw=True)
+        data = self._exec(action="qstat", args=job_id)
         return JobStat(**data)
 
-    def qsub(self, props: JobSubmit) -> None:
-        exe = os.path.join(self._bin, "qsub")
-        cmd = " ".join((exe, props.to_qsub()))
-        shell(command=cmd)
+    def qsub(self, props: JobSubmit) -> str:
+        return self._exec(action="qsub", args=props.to_qsub())
+
+    def _exec(self, action, args):
+        exe = os.path.join(self.env["PBS_EXEC"], "bin", action)
+        cmd = " ".join((exe, args))
+        return shell(command=cmd).output(raw=True)
