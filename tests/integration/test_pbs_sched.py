15452c6e7ca69dc67deb09a058bd9d04b420f780
Renato Damas <me@renatodamas.com>
2023-02-21 17:55:54 +0000

    test(integration): test 200 case for qsub #wip

--- tests/integration/test_pbs_sched.py
+++ tests/integration/test_pbs_sched.py
@@ -2,6 +2,17 @@ from base64 import b64encode
 
 import pytest
 
+from src.services.pbs import PBS
+
+
+@pytest.fixture(scope="class", autouse=True)
+def pbs(class_mocker):
+    class_mocker.patch("src.api.pbs.PBS", return_value=PBS(env={
+        "PBS_EXEC": "/opt/pbs/2022",
+        "PBS_HOME": "/opt/pbs/2022/var/spool",
+        "PBS_SERVER": "pbs00",
+    }))
+
 
 @pytest.fixture()
 def auth(app, mocker):
@@ -15,9 +26,8 @@ class TestPBSQsubPOST:
         response = client.post("/pbs/qsub", headers={})
         assert response.status_code == 401
 
-    def test_valid_job_returns_200(self, client, auth, qsub_job):
+    def test_valid_job_returns_200(self, client, auth, qsub_job, mock_shell):
+        mock_shell.configure_mock(**{"output.return_value": "100.pbs00"})
         response = client.post("/pbs/qsub", headers=auth, json=qsub_job.dict())
-        print(response.json)
         assert response.status_code == 200
-        assert response.text == ""
-        assert response.json == ["file.txt"]
+        assert response.json == {"job_id": "100.pbs00"}
