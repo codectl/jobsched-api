15bb76aa03a50e54141eec72d629e52cdacfff2b
Renato Damas <me@renatodamas.com>
2023-02-22 17:21:45 +0000

    test(integration): add tests to qstat #wip

--- tests/integration/test_pbs_sched.py
+++ tests/integration/test_pbs_sched.py
@@ -1,3 +1,4 @@
+import json
 from base64 import b64encode
 
 import pytest
@@ -19,6 +20,23 @@ def auth(app, mocker):
     mocker.patch("src.api.auth.load_user", return_value=None)
     mocker.patch("src.services.auth.AuthSvc.authenticate", return_value=True)
     return {"Authorization": f"Basic {b64encode(b'user:pass').decode()}"}
+
+
+class TestPBSQstatGET:
+    def test_valid_job_id_returns_200(self, client, auth, qstat_data, qstat_job,
+                                      mock_shell):
+        mock_shell.configure_mock(**{"output.return_value": qstat_data})
+        response = client.get("/pbs/qstat/100.pbs00", headers=auth)
+        assert response.status_code == 200
+        assert response.json == json.loads(qstat_job.json())
+
+    def test_unauthorized_request_throws_401(self, client):
+        response = client.get("/pbs/qstat/100.pbs00", headers={})
+        assert response.status_code == 401
+
+    def test_disallowed_method_throws_405(self, client):
+        response = client.post("/pbs/qstat/100.pbs00")
+        assert response.status_code == 405
 
 
 class TestPBSQsubPOST:
