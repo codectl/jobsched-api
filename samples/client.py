b964c7aecaa772bfdeb9a6f75c97f741f0fdd104
Renato Damas <me@renatodamas.com>
2023-03-27 12:37:33 -0400

    chore: create samples dir to contain code examples

---
+++ samples/client.py
@@ -0,0 +1,30 @@
+import os
+
+import requests
+
+
+# edit these variables accordingly
+base_url = "http://localhost:5000/api/jobsched/v1"
+username = os.environ["USERNAME"]
+password = os.environ["PASSWORD"]
+
+# POST request to create job
+r = requests.post(
+    url=f"{base_url}/pbs",
+    headers={"accept": "application/json"},
+    auth=(username, password),
+    data={
+        "name": "STDIN",
+        "queue": "testq",
+        "submit_args": "-- /bin/sleep 10",
+        "resources": {
+            "mem": "10gb",
+            "cpu": 1,
+            "gpu": 1,
+            "node_count": 1,
+            "place": "pack",
+            "walltime": "00:02:00",
+        },
+    }
+)
+assert r.status_code == 200
